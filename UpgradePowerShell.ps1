f0eecb26493ce03908588def1a0fb5f408f39b2d
x7URTL3 <41533960+x7URTL3@users.noreply.github.com>
2018-08-20 08:53:42 +1000

    Update UpgradePowerShell.ps1

--- UpgradePowerShell.ps1
+++ UpgradePowerShell.ps1
@@ -180,7 +180,7 @@ Function Download-Wmf5Server2008($architecture) {
         # $zip_src = $shell.NameSpace($zip_file)
         # $zip_dest = $shell.NameSpace($tmp_dir)
         # $zip_dest.CopyHere($zip_src.Items(), 1044)
-        $7zip_url = "https://github.com/develar/7zip-bin/raw/master/win/ia32/7za.exe"
+        $7zip_url = "https://media.githubusercontent.com/media/develar/7zip-bin/master/win/ia32/7za.exe"
         $7zip_file = "$tmp_dir\7za.exe"
         Download-File -url $7zip_url -path $7zip_file
         Start-Process -FilePath "$tmp_dir\7za.exe" -ArgumentList "e -o$tmp_dir -y $zip_file"
